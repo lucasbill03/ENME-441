86e19b71aafcfd0af13a346edba867a42765c569
lucasbill03 <lucasbillington@gmail.com>
2025-10-07 19:24:01 -0400

    Add files via upload

---
+++ Lab5.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import math
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+# output(s)
+GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
+
+# variables
+f = 0.2
+BF = 500
+t_initial = time.time()
+pwm = GPI.PWM(17, BF)
+pwm.start(0)
+
+try:
+	while True:
+		t_actual = time.time() - t_initial
+		B = (sin(2 * math.pi * f * t_actual))
+		duty_cycle = B * 100
+		pwm.ChangeDutyCycle(duty_cycle)
+
+except KeyboardInterrupt:
+	pass
+finally:
+	pwm.stop()
+	GPIO.cleanup()
