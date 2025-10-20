0d22c0908da081832836fc3da50240d2e39512f8
lucasbill03 <lucasbillington@gmail.com>
2025-10-20 14:55:32 -0400

    Add files via upload

---
+++ Lab6Test.py
@@ -0,0 +1,17 @@
+import time, RPi.GPIO as GPIO
+from Lab6Shifter import Shifter
+
+sh = Shifter(23, 25, 24)
+try:
+
+    for i in range(8):
+
+        sh.shiftByte(1 << i)
+        time.sleep(0.2)
+    sh.shiftByte(0)
+
+    for i in range 
+
+finally:
+
+    GPIO.cleanup()
