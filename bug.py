f7660947fe21cef020685988b8d6246d70e48c4b
lucasbill03 <lucasbillington@gmail.com>
2025-10-20 15:50:11 -0400

    Update bug.py

--- bug.py
+++ bug.py
@@ -10,22 +10,17 @@ GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 bug = Bug(timestep=0.1, x=3, isWrapOn=False)
 
-runner = None
-is_running = False
-prev_s1 = False
-prev_s2 = False
-
 try:
 	while True:
 		if GPIO.input(17):
 			bug.start()
-		else:
-			bug.stop()
-			
-		if GPIO.input(27):
-			bug.isWrapOn = True
-		else:
-			bug.isWrapOn = False
+        else:
+            bug.stop()
+
+        if GPIO.input(27):
+        	bug.isWrapOn = True
+        else:
+        	bug.isWrapOn = False
 		
 		if GPIO.input(22):
 			bug.timestep = 0.03
@@ -35,12 +30,8 @@ try:
 		time.sleep(0.05)
 
 except KeyboardInterrupt:
-
 	pass
 	
 finally:
 	bug.stop()
-
 	GPIO.cleanup()
-
-
