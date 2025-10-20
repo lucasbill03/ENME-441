d1503c72e3db1b1584080ffe633df3863bff88b9
lucasbill03 <lucasbillington@gmail.com>
2025-10-20 13:15:07 -0400

    Add files via upload

---
+++ Lab6Shifter.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+## self will be the reference to the instance of the class
+class Shifter:
+
+	## Initializer for the Shifter class, field values set to the
+	## serialPin, clockPin, and latchPin values
+    def __init__(self, serialPin, clockPin, latchPin):
+        self.serialPin = serialPin
+        self.clockPin = clockPin
+        self.latchPin = latchPin
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.serialPin, GPIO.OUT)
+        GPIO.setup(self.clockPin, GPIO.OUT, initial=0)
+        GPIO.setup(self.latchPin, GPIO.OUT, initial=0)
+
+    def __ping(self, pin):
+        GPIO.output(pin, 1)
+        time.sleep(0)
+        GPIO.output(pin, 0)
+
+    def shiftByte(self, dataByte):
+        for i in range(8):
+            bit = (dataByte >> i) & 1
+            GPIO.output(self.serialPin, bit)
+            self.__ping(self.clockPin)
+        self.__ping(self.latchPin)
